d880c8f9d145ab2b975a0c4ffb4656d1096767a5
ciyer64 <curreniyer@college.harvard.edu>
2016-04-18 14:49:34 -0400

    coding started

---
+++ kk.c
@@ -0,0 +1,14 @@
+///////////////////////////////////////
+/*									 */
+/*  CS124 Programming Assignment 3   */
+/*     Curren Iyer & Paul Lisker     */
+/*			 April 18, 2016			 */
+/*	   								 */
+///////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
