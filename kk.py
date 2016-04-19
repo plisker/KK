c0092864dbe364a248cf8d122af7ca79600f5c18
ciyer64 <curreniyer@college.harvard.edu>
2016-04-19 01:13:20 -0400

    that moment when you've written almost the entire pset, but does it work?

--- kk.py
+++ kk.py
@@ -1,6 +1,5 @@
 import random
 import math
-import copy
 
 max_iter = 25000
 
@@ -13,6 +12,7 @@ def T(iter):
 def gen_sol_rm(n):
 	S=[]
 	for i in range(n):
+		#50-50 chance of appending 1 or -1
 		if random.random() < 0.5:
 			S.append(1)
 		else:
@@ -21,10 +21,13 @@ def gen_sol_rm(n):
 
 #Find neighbor
 def neighbor_rm(S):
-	i,j=0,0
+	i,j=random.randint(0,len(S)-1),random.randint(0,len(S)-1)
+	#ensures i and j are different
 	while i==j:
 		i,j=random.randint(0,len(S)-1),random.randint(0,len(S)-1)
 	S[i]*=-1
+	#50% chance of multiplying just S[i] by -1
+	#50% chance of multiplying S[i] and S[j] by -1
 	if random.random()<0.5:
 		S[j]*=-1
 	return S
@@ -37,29 +40,34 @@ def res_rm(S,A):
 def repeat_rand_rm(A):
 	S=gen_sol_rm(len(A))
 	for i in range(max_iter):
+		#generate random solution S
 		S_0=gen_sol_rm(len(A))
+		#compare residues, replace if lower
 		if res_rm(S_0,A)<res_rm(S,A):
-			S=copy.deepcopy(S_0)
+			S=S_0
 	return S
 
 #Hill climbing
 def hill_climb_rm(A):
 	S=gen_sol_rm(len(A))
 	for i in range(max_iter):
-		S_0=neighbor_rm(copy.deepcopy(S))
+		#generate random neighbor
+		S_0=neighbor_rm(S)
+		#compare residues, replace if lower
 		if res_rm(S_0,A)<res_rm(S,A):
-			S=copy.deepcopy(S_0)
+			S=S_0
 	return S
 
 #Simulated annealing
 def simul_anneal_rm(A):
 	S=gen_sol_rm(len(A))
-	S_1=copy.deepcopy(S)
+	S_1=S
 	for i in range(max_iter):
-		S_0=random_move(copy.deepcopy(S))
+		S_0=neighbor_rm(S)
 		if res_rm(S_0,A)<res_rm(S,A):
 			S=S_0
-		elif random.random<math.exp(-(res_rm(S_0,A)-res_rm(S,A))/T(iter)):
+		#replace w/ probability e^((res(S_0)-res(S))/T(i))
+		elif random.random<math.exp(-(res_rm(S_0,A)-res_rm(S,A))/T(i)):
 			S=S_0
 		if res_rm(S,A)<res_rm(S_1,A):
 			S_1=S
@@ -70,18 +78,57 @@ def simul_anneal_rm(A):
 def gen_sol_p(n):
 	S=[]
 	for i in range(n):
-		S.append(random.randint(1,n-1))
+		#append random value 1<=x<=n
+		S.append(random.randint(1,n))
 	return S
 
 #Find neighbor
 def neighbor_p(S):
-	
+	i,j=random.randint(0,len(S)-1),random.randint(0,len(S)-1)
 
 #Calculate residue given input A and coefficient S
+def res_p(S,A):
+	A_0=[]
+	for i in range(len(A)):
+		A_0.append(0)
+	for j in range(len(A)):
+		A_0[j] += A[j]
 
 #Repeated random
+def repeat_rand_p(A):
+	S=gen_sol_p(len(A))
+	for i in range(max_iter):
+		S_0 = gen_sol_p(len(A))
+		if res_p(S_0,A)<res_p(S,A):
+			S=S_0
+	return S
 
 #Hill climbing
+def hill_climb_p(A):
+	S=gen_sol_p(len(A))
+	for i in range(max_iter):
+		S_0=neighbor_p(S)
+		if res_p(S_0,A)<res_p(S,A):
+			S=S_0
+	return S
 
 #Simulated annealing
+def simul_anneal_p(A):
+	S=gen_sol_p(len(A))
+	S_1=S
+	for i in range(max_iter):
+		S_0=neighbor_p(S)
+		if res_p(S_0,A)<res_p(S,A):
+			S=S_0
+		#replace w/ probability e^((res(S_0)-res(S))/T(i))
+		elif random.random<math.exp(-(res_p(S_0,A)-res_p(S,A))/T(i)):
+			S=S_0
+		if res_p(S,A)<res_p(S_1,A):
+			S_1=S
+	return S_1
+
+
+#Karmarkar-Karp Algorithm
+def kk(A):
+	A.sort():
 
